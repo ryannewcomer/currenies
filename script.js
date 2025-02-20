8bb48d58fbce81313d1a862ed074fd63925411e3
Ryan Hung <ryanhungryan@gmail.com>
2025-02-20 10:40:15 +0000

    add javascipt to display map on index. update euro and lebanese page

---
+++ script.js
@@ -0,0 +1,35 @@
+function uk() {
+  document.getElementById("ukSmall").style.display = "inline";
+  document.getElementById("usSmall").style.display = "none";
+  document.getElementById("lebanonSmall").style.display = "none";
+  document.getElementById("kuwaitSmall").style.display = "none";
+  document.getElementById("euSmall").style.display = "none";
+}
+function us() {
+  document.getElementById("ukSmall").style.display = "none";
+  document.getElementById("usSmall").style.display = "inline";
+  document.getElementById("lebanonSmall").style.display = "none";
+  document.getElementById("kuwaitSmall").style.display = "none";
+  document.getElementById("euSmall").style.display = "none";
+}
+function lebanon() {
+  document.getElementById("ukSmall").style.display = "none";
+  document.getElementById("usSmall").style.display = "none";
+  document.getElementById("lebanonSmall").style.display = "inline";
+  document.getElementById("kuwaitSmall").style.display = "none";
+  document.getElementById("euSmall").style.display = "none";
+}
+function kuwait() {
+  document.getElementById("ukSmall").style.display = "none";
+  document.getElementById("usSmall").style.display = "none";
+  document.getElementById("lebanonSmall").style.display = "none";
+  document.getElementById("kuwaitSmall").style.display = "inline";
+  document.getElementById("euSmall").style.display = "none";
+}
+function eu() {
+  document.getElementById("ukSmall").style.display = "none";
+  document.getElementById("usSmall").style.display = "none";
+  document.getElementById("lebanonSmall").style.display = "none";
+  document.getElementById("kuwaitSmall").style.display = "none";
+  document.getElementById("euSmall").style.display = "inline";
+}
